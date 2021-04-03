79e412d3f138d26806048e0a6f93c108d2a47f4c
Philip Zhang <zhangxilai@doublechaintech.com>
2021-04-04 00:05:06 +0800

    trying to fix the windows size changing issue when taking screen shot

--- freshchain.js
+++ freshchain.js
@@ -20,28 +20,28 @@ console.log('myArgs: ', args);
   const url = webSocketDebuggerUrl 
   const browser = await puppeteer.connect({ browserWSEndpoint: url });	
   const page = await browser.newPage();
-  await page.setViewport({ width: 1366, height: 768})
+  await page.setViewport({ width: 1200, height: 800})
   //await page.setDefaultNavigationTimeout(1000000);
   await page.setDefaultNavigationTimeout(0);
   await page.setViewport({
-    width: 1920,
-    height: 1080 
+    width: 1200,
+    height: 800 
 })
 
   await page.goto('https://demo.doublechaintech.com/admin/freshchain/',{ waitUntil: 'networkidle0'});
-  await page.screenshot({path: 'login-page.png'});
+  //await page.screenshot({path: 'login-page.png'});
   await page.type('#username', 'SU000001');
   await page.type('#password', 'admin123');
 
-  await page.screenshot({path: 'home00.png'});
+  //await page.screenshot({path: 'home00.png'});
   await page.click('button[type=submit');
 
-  await page.screenshot({path: 'home01.png'});
+  //await page.screenshot({path: 'home01.png'});
   await page.waitForSelector('body', {visible: true})
-  await page.screenshot({path: 'home02.png'});
+  //await page.screenshot({path: 'home02.png'});
   await page.hover('.icon-item-box:first-child');
   await page.waitForSelector('body', {visible: true})
-  await page.screenshot({path: 'home03.png'});
+  //await page.screenshot({path: 'home03.png'});
   await page.click('.icon-item-box:first-child');
   await page.waitForSelector('body', {visible: true})
   await page.waitForTimeout(2000);
